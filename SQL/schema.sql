0f1789d30e1fada1a96cbc707331826b11895b30
Marco Petri <marcoPetriPolimi@users.noreply.github.com>
2020-11-26 21:33:34 +0100

    Some triggers created and schema updated
    Schema compatible with MySQL

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -15,12 +15,14 @@ CREATE SCHEMA db2_project DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 --
 CREATE TABLE IF NOT EXISTS User (
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	nickname VARCHAR(25) NOT NULL UNIQUE KEY,
+	nickname VARCHAR(25) NOT NULL,
 	password VARCHAR(50) NOT NULL,
 	email VARCHAR(100) NOT NULL,
+	registration DATE NOT NULL,
 	points INT UNSIGNED,
 	blocked BOOL NOT NULL,
 	role INT UNSIGNED NOT NULL,
+	UNIQUE KEY(nickname),
 	CHECK(role = 1 OR role = 2)
 ) AUTO_INCREMENT = 1;
 
@@ -30,7 +32,8 @@ CREATE TABLE IF NOT EXISTS User (
 CREATE TABLE IF NOT EXISTS Questionnaire (
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	date DATE NOT NULL,
-	name VARCHAR(50) NOT NULL UNIQUE KEY
+	name VARCHAR(50) NOT NULL,
+	UNIQUE KEY(name)
 ) AUTO_INCREMENT = 1;
 
 --
@@ -48,12 +51,11 @@ CREATE TABLE IF NOT EXISTS Question (
 -- TABLE FOR PRODUCT ANSWERS
 --
 CREATE TABLE IF NOT EXISTS ProductAnswer (
-	id INT UNSIGNED,
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	questionnaireId INT UNSIGNED NOT NULL,
 	questionId INT UNSIGNED NOT NULL,
 	userId INT UNSIGNED NOT NULL,
 	word VARCHAR(50) NOT NULL,
-	PRIMARY KEY (id),
 	FOREIGN KEY (questionnaireId) REFERENCES Questionnaire(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (questionId) REFERENCES Question(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (userId) REFERENCES User(id) ON UPDATE CASCADE ON DELETE CASCADE
@@ -64,7 +66,8 @@ CREATE TABLE IF NOT EXISTS ProductAnswer (
 --
 CREATE TABLE IF NOT EXISTS OffensiveWord (
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	word VARCHAR(50) NOT NULL UNIQUE KEY
+	word VARCHAR(50) NOT NULL,
+	UNIQUE KEY(word)
 ) AUTO_INCREMENT = 1;
 
 
@@ -80,10 +83,9 @@ CREATE TABLE IF NOT EXISTS OffensiveWord (
 -- TABLE FOR POSSIBLE ANSWERS
 --
 CREATE TABLE IF NOT EXISTS PossibleAnswer (
-	id INT UNSIGNED AUTO_INCREMENT,
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	questionId INT UNSIGNED,
 	word VARCHAR(50) NOT NULL,
-	PRIMARY KEY (id,questionId),
 	FOREIGN KEY (questionId) REFERENCES Question(id) ON UPDATE CASCADE ON DELETE CASCADE
 ) AUTO_INCREMENT = 1;
 
@@ -93,41 +95,42 @@ CREATE TABLE IF NOT EXISTS PossibleAnswer (
 -- Additional info Expertise: 0 = none, 1 = low, 2 = medium, 3 = high
 --
 CREATE TABLE IF NOT EXISTS PersonalAnswer (
-	id INT UNSIGNED,
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	questionnaireId INT UNSIGNED,
 	userId INT UNSIGNED,
 	age SMALLINT UNSIGNED,
 	sex CHAR(1),
 	expertise SMALLINT UNSIGNED,
-	PRIMARY KEY (id),
     UNIQUE KEY(questionnaireId,userId),
 	FOREIGN KEY (questionnaireId) REFERENCES Questionnaire(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (userId) REFERENCES User(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	CHECK(sex = 'M' OR sex = 'F' OR sex = 'U'),
 	CHECK(expertise = 0 OR expertise = 1 OR expertise = 2 OR expertise = 3)
-);
+) AUTO_INCREMENT = 1;
 
 --
 -- TABLE FOR QUESTIONNAIRES CREATIONS
 --
 CREATE TABLE IF NOT EXISTS Creation (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	questionnaireId INT UNSIGNED,
 	creatorId INT UNSIGNED,
-	PRIMARY KEY (questionnaireId,creatorId),
+	UNIQUE KEY(questionnaireId,creatorId),
 	FOREIGN KEY (questionnaireId) REFERENCES Questionnaire(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (creatorId) REFERENCES User(id) ON UPDATE CASCADE ON DELETE CASCADE
-);
+) AUTO_INCREMENT = 1;
 
 --
 -- TABLE FOR QUESTIONNAIRE SUBMISSIONS
 --
 CREATE TABLE IF NOT EXISTS Submission (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	questionnaireId INT UNSIGNED,
 	userId INT UNSIGNED,
 	submitted BOOL NOT NULL,
 	points INT UNSIGNED NOT NULL,
 	date DATETIME NOT NULL,
-	PRIMARY KEY (questionnaireId,userId),
+    UNIQUE KEY(questionnaireId,userId),
 	FOREIGN KEY (questionnaireId) REFERENCES Questionnaire(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (userId) REFERENCES User(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -136,9 +139,120 @@ CREATE TABLE IF NOT EXISTS Submission (
 -- TABLE FOR INCLUSION RELATION BETWEEN QUESTIONS AND QUESTIONNAIRES
 --
 CREATE TABLE IF NOT EXISTS Inclusion (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	questionnaireId INT UNSIGNED,
 	questionId INT UNSIGNED,
-	PRIMARY KEY (questionnaireId,questionId),
+    UNIQUE KEY(questionnaireId,questionId),
 	FOREIGN KEY (questionnaireId) REFERENCES Questionnaire(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (questionId) REFERENCES Question(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+
+
+--------------------------------------------------
+--												--
+--												--
+--	TRIGGERS ON THE DATABASE ARE HERE BELOW		--
+--												--
+--												--
+--------------------------------------------------
+DELIMITER $$
+
+CREATE TRIGGER QuestionnairesHaveOneCreator
+-- TODO
+BEGIN
+	-- TODO
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER QuestionnairesCreatorIsAdministrator
+-- TODO
+BEGIN
+	-- TODO
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER UserDoesQuestionnairesAfterRegistration
+AFTER INSERT ON Submission
+FOR EACH ROW
+BEGIN
+	DECLARE regDate DATE;
+
+	SELECT registration into regDate FROM User WHERE id = NEW.userId;
+
+	IF (NEW.date < regDate) THEN
+		SIGNAL SQLSTATE 'HV000' SET MESSAGE_TEXT = 'Users cannot fill questionnaires before registration.';
+	END IF;
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER AdministratorCreatesQuestionnairesAfterRegistration
+-- TODO
+BEGIN
+	-- TODO
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER NicknamesDoNotContainOffensiveWord
+-- TODO
+BEGIN
+	-- TODO
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER QuestionnairesResponsesDoNotContainOffensiveWord
+-- TODO
+BEGIN
+	-- TODO
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER QuestionnairesResponsesAreEqualToQuestions
+-- TODO
+BEGIN
+	-- TODO
+END$$
+
+DELIMITER ;
+
+DELIMITER $$
+
+CREATE TRIGGER ResponseNumberForAQuestionIsPositiveAndLessThanMaximum
+AFTER INSERT ON ProductAnswer
+FOR EACH ROW
+BEGIN
+	DECLARE maximumResponses, givenResponses, responseType INT;
+
+	SELECT count(*) INTO maximumResponses FROM PossibleAnswer WHERE questionId = NEW.questionId;
+	SELECT count(*) INTO givenResponses FROM ProductAnswer WHERE questionId = NEW.questionId AND questionnaireId = NEW.questionnaireId AND userId = NEW.userId;
+	SELECT type INTO responseType FROM Question WHERE id = NEW.questionId;
+
+	IF (responseType = 1) THEN
+		IF (givenResponses < 1 OR givenResponses > maximumResponses) THEN
+			SIGNAL SQLSTATE 'HV000' SET MESSAGE_TEXT = 'Responses to a multiple question must be less than the maximum number of responses and at least one.';
+		END IF;
+	ELSE
+		IF (givenResponses <> 1) THEN
+			SIGNAL SQLSTATE 'HV000' SET MESSAGE_TEXT = 'Response to a single question must be unique.';
+		END IF;
+	END IF;
+END$$
+
+DELIMITER ;
