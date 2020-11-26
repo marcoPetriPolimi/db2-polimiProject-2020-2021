729218650bfef2fb7a24bb3b1c59ef5a82920aaf
Marco Petri <marcoPetriPolimi@users.noreply.github.com>
2020-11-26 22:51:51 +0100

    User and submission ejb

--- CODE/PetriPinariRomeoSbrolli/src/ejb/database/Submission.java
+++ CODE/PetriPinariRomeoSbrolli/src/ejb/database/Submission.java
@@ -1,8 +1,83 @@
 package ejb.database;
 
+import java.io.Serializable;
+import java.util.Date;
+
 import javax.persistence.*;
 
+/**
+ * This class is the EJB for the submission database class.
+ * @author Marco Petri
+ */
 @Entity
-public class Submission {
-
+@Table(name = "submission", schema = "db2_project")
+public class Submission implements Serializable {
+	private static final long serialVersionId = 1;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@OneToOne
+	@JoinColumn(name="questionnaireId")
+	private Questionnaire questionnaire;
+	@ManyToOne
+	@JoinColumn(name="userId")
+	private User senderUser;
+	private boolean submitted;
+	/**
+	 * The points column of the database can be null, so an object is needed because a primitive type cannot be null.
+	 */
+	private Integer points;
+	/**
+	 * Usage of the annotation Temporal is needed to translate a java.util.Date object type (https://javaee.github.io/javaee-spec/javadocs/javax/persistence/Temporal.html).
+	 */
+	@Temporal(TemporalType.DATE)
+	private Date date;
+	
+	public Submission() {}
+	public Submission(Questionnaire questionnaire, User senderUser, boolean submitted, Integer points, Date date) {
+		this.questionnaire		= questionnaire;
+		this.senderUser			= senderUser;
+		this.submitted			= submitted;
+		this.points				= points;
+		this.date				= date;
+	}
+	
+	/* ******************
+	 * 		SETTERS		*
+	 ********************/
+	public void setQuestionnaire(Questionnaire questionnaire) {
+		this.questionnaire		= questionnaire;
+	}
+	public void setUser(User senderUser) {
+		this.senderUser			= senderUser;
+	}
+	public void setSubmitted(boolean submitted) {
+		this.submitted			= submitted;
+	}
+	public void setPoints(Integer points) {
+		this.points				= points;
+	}
+	public void setDate(Date date) {
+		this.date				= date;
+	}
+	
+	/* ******************
+	 * 		GETTERS		*
+	 ********************/
+	public Questionnaire getQuestionnaire() {
+		return questionnaire;
+	}
+	public User getUser() {
+		return senderUser;
+	}
+	public boolean getSubmitted() {
+		return submitted;
+	}
+	public Integer getPoints() {
+		return points;
+	}
+	public Date getDate() {
+		return date;
+	}
 }
--- CODE/PetriPinariRomeoSbrolli/src/ejb/database/User.java
+++ CODE/PetriPinariRomeoSbrolli/src/ejb/database/User.java
@@ -1,8 +1,103 @@
 package ejb.database;
 
-import javax.persistence.Entity;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.*;
 
+/**
+ * This class is the EJB for the user database class.
+ * @author Marco Petri
+ */
 @Entity
-public class User {
+@Table(name = "user", schema = "db2_project")
+public class User implements Serializable {
+	private static final long serialVersionId = 1;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(unique=true)
+	private String nickname;
+	private String password;
+	private String email;
+	/**
+	 * Usage of the annotation Temporal is needed to translate a java.util.Date object type (https://javaee.github.io/javaee-spec/javadocs/javax/persistence/Temporal.html).
+	 */
+	@Temporal(TemporalType.DATE)
+	private Date registration;
+	/**
+	 * The points column of the database can be null, so an object is needed because a primitive type cannot be null.
+	 */
+	private Integer points;
+	private boolean blocked;
+	private int role;
+	
+	/**
+	 * Reverse relationship for the 1:N relationship with the submissions.
+	 */
+	@OneToMany(mappedBy="senderUser")
+	private Collection<Submission> submissions;
+	
+	public User() {
+	}
+	public User(String nickname, String password, String email, Date registration, Integer points, boolean blocked, int role) {
+		this.nickname		= nickname;
+		this.password		= password;
+		this.email			= email;
+		this.registration	= registration;
+		this.points			= points;
+		this.blocked		= blocked;
+		this.role			= role;
+	}
+	
+	/* ******************
+	 * 		SETTERS		*
+	 ********************/
+	public void setNickname(String nickname) {
+		this.nickname		= nickname;
+	}
+	public void setPassword(String password) {
+		this.password		= password;
+	}
+	public void setEmail(String email) {
+		this.email			= email;
+	}
+	public void setRegistration(Date registration) {
+		this.registration	= registration;
+	}
+	public void setPoints(Integer points) {
+		this.points			= points;
+	}
+	public void setBlocked(boolean blocked) {
+		this.blocked		= blocked;
+	}
+	public void setRole(int role) {
+		this.role			= role;
+	}
+	
+	/* ******************
+	 * 		GETTERS		*
+	 ********************/
+	public String getNickname() {
+		return nickname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public Date getRegistration() {
+		return registration;
+	}
+	public Integer getPoints() {
+		return points;
+	}
+	public boolean getBlocked() {
+		return blocked;
+	}
+	public int getRole() {
+		return role;
+	}
 }
