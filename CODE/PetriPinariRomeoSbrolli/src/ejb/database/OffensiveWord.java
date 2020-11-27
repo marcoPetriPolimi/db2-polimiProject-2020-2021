372b7a7f8eb1efe2d4c82a706a0ce64786004a5d
EtionPinari <etion.pinari@mail.polimi.it>
2020-11-27 14:16:14 +0100

    Added 3 classes of the model of the DB

--- CODE/PetriPinariRomeoSbrolli/src/ejb/database/OffensiveWord.java
+++ CODE/PetriPinariRomeoSbrolli/src/ejb/database/OffensiveWord.java
@@ -2,7 +2,42 @@ package ejb.database;
 
 import javax.persistence.*;
 
-@Entity
-public class OffensiveWord {
+// The persistent class for the offensiveWord database table.
+// @author Etion
 
+@Entity
+@Table(name = "OffensiveWord", schema = "db2_project")
+public class OffensiveWord {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column(unique=true)
+	private String word;
+	
+	
+	
+	/*****	Setters *****/
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setWord(String word) {
+		this.word = word;
+	}
+	
+	
+	
+	/*****	Getters *****/
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public String getWord() {
+		return this.word;
+	}
+	
 }
--- CODE/PetriPinariRomeoSbrolli/src/ejb/database/Question.java
+++ CODE/PetriPinariRomeoSbrolli/src/ejb/database/Question.java
@@ -2,7 +2,46 @@ package ejb.database;
 
 import javax.persistence.*;
 
+//The persistent class for the question database table.
+//@author Etion
+
 @Entity
+@Table(name = "question", schema = "db2_project")
 public class Question {
 
+	@Id
+	private int id;
+	
+	private String question;
+	
+	private short type;
+	
+	/*****	Setters *****/
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	
+	public void setType(short type) {
+		this.type = type;
+	}
+	
+	
+	/*****	Getters *****/
+	
+	public int getId() {
+		return this.id ;
+	}
+	
+	public String getQuestion() {
+		return this.question;
+	}
+	
+	public short getType() {
+		return this.type;
+	}
 }
