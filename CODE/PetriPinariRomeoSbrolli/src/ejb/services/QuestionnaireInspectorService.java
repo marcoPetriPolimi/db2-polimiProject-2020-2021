586414edc57fe349eb510f69b62adb6e13c792d6
Cristian Sbrolli <cristian.sbrolli@mail.polimi.it>
2020-12-03 00:24:18 +0100

    Update QuestionnaireInspectorService.java

--- CODE/PetriPinariRomeoSbrolli/src/ejb/services/QuestionnaireInspectorService.java
+++ CODE/PetriPinariRomeoSbrolli/src/ejb/services/QuestionnaireInspectorService.java
@@ -1,10 +1,14 @@
 package ejb.services;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.ejb.*;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Tuple;
+
 import ejb.database.*;
 
 
@@ -22,11 +26,10 @@ public class QuestionnaireInspectorService {
 		return submissions;
 	}
 	
-	public List<ProductAnswer> getQuestionnaireProductAnswers(int questionnaireId){
-		Questionnaire questionnaire = em.find(Questionnaire.class, questionnaireId);
-	
-		return null;
+	public Map<Integer,String> getQuestionnaireProductAnswers(int questionnaireId){
+		Map<Integer,String> answers= em
+				.createQuery("SELECT u.Id,u.nickname from User u, Submission s WHERE u.Id = s.userId AND s.questionnaireId = :qId ORDER BY u.Id DESC", Tuple.class)
+				.setParameter("qId",questionnaireId).getResultStream().collect(Collectors.toMap(tuple -> ((Number) tuple.get(0)).intValue(), tuple-> ((String) tuple.get(1))));
+		return answers;
 	}
-	
-	
 }
