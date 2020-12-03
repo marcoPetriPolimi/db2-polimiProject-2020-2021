5885011dc5be888d6ffb5d2fd549a7d421d3481c
Marco Petri <marcoPetriPolimi@users.noreply.github.com>
2020-12-03 12:19:22 +0100

    Filters, Servlets and xml
    Filters defined to manage access to pages using RBAC, servlets created and not implemented, web.xml initial version created

---
+++ CODE/PetriPinariRomeoSbrolli/src/web/controllers/GetLogout.java
@@ -0,0 +1,23 @@
+package web.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/logout")
+public class GetLogout extends HttpThymeleafServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// insert the code for the get
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
---
+++ CODE/PetriPinariRomeoSbrolli/src/web/filters/CustomerFilter.java
@@ -0,0 +1,34 @@
+package web.filters;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import ejb.database.User;
+
+@WebFilter
+public class CustomerFilter extends HttpFilter {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse resp = (HttpServletResponse)response;
+		HttpSession session = req.getSession();
+		User user = (User) session.getAttribute("user");
+
+		if (user.getRole() != 1) {
+			resp.sendRedirect("/homepage");
+		} else {
+			chain.doFilter(request,response);
+		}
+	}
+}
