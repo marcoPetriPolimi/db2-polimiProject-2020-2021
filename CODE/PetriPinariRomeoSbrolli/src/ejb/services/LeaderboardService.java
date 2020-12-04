db9dfc10d16ac11fc4a610d882019ed1f779c4aa
GiorgioRomeo98 <giorgioromeo98@gmail.com>
2020-12-05 00:29:15 +0100

    Create LeaderboardService.java

---
+++ CODE/PetriPinariRomeoSbrolli/src/ejb/services/LeaderboardService.java
@@ -0,0 +1,38 @@
+package ejb.services;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import ejb.database.Submission;
+import ejb.database.User;
+
+@Stateless
+public class LeaderboardService {
+	
+	@PersistenceContext(unitName = "PetriPinariRomeoSbrolliEJB")
+	private EntityManager em;
+	
+	public LeaderboardService() {
+	}
+	
+	//TODO Define in a better way (add functional programming to return a map)
+	public List<User> getGeneralLeaderboard(){
+		List<User> generalLeaderboard = em
+				.createQuery("SELECT u FROM User u ORDER BY u.points DESC", User.class).getResultList();
+
+		return generalLeaderboard;
+	}
+	
+	//TODO Define in a better way (add functional programming to return a map)
+		public List<Submission> getQuestionnaireLeaderboard(int questionnaireId){
+			List<Submission> questionnaireLeaderboard = em
+					.createQuery("SELECT s FROM Submission s WHERE s.questionnaireId = :qId "
+							   + "ORDER BY s.points DESC", Submission.class).setParameter("qId", questionnaireId).getResultList();
+
+			return questionnaireLeaderboard;
+		}
+
+}
