77a4f86d2b01f43391dfc1284535533d7640ef4c
Marco Petri <marcoPetriPolimi@users.noreply.github.com>
2020-12-19 12:48:39 +0100

    Web page format

--- CODE/PetriPinariRomeoSbrolliWeb/src/web/controllers/GetHomepage.java
+++ CODE/PetriPinariRomeoSbrolliWeb/src/web/controllers/GetHomepage.java
@@ -1,11 +1,18 @@
 package web.controllers;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.context.WebContext;
+
+import database.User;
 
 @WebServlet("/homepage")
 public class GetHomepage extends HttpThymeleafServlet {
@@ -13,7 +20,17 @@ public class GetHomepage extends HttpThymeleafServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// insert the code for the get
+		ResourceBundle lang = findLanguage(req);
+		ServletContext context = getServletContext();
+		WebContext webContext = new WebContext(req,resp,context);
+		String page = "homepage";
+		
+		HttpSession session = req.getSession();
+		User user = (User) session.getAttribute("user");
+		
+		webContext.setVariable("lang", lang);
+		webContext.setVariable("user", user);
+		thymeleaf.process(page,webContext,resp.getWriter());
 	}
 	
 	@Override
